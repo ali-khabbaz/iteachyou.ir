84cee3210c115072c89bcdad528c421e14ef034d
ali <ali.khabbaz14@gmail.com>
2015-09-05 18:45:51 +0430

    reza gir ok_2

---
+++ test.sql
@@ -0,0 +1,78 @@
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,1,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Overview.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,2,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Ionic Command Line Features.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,3,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Installing Ionic.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,4,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Starting a New Project.mp4', 'Pluralsight', 'Steve Michelotti');
+
+
+
+
+
+
+
+
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,5,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Run the App With a Browser.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,6,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Run the App With an Emulator.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,7,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Run the App on a Mobile Device.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,8,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Ionic Starter Templates.mp4', 'Pluralsight', 'Steve Michelotti');
+
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,9,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Setting Up a Development Environment With Sublime Text.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,10,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Setting Up a Development Environment With Visual Studio.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,11,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Setting Up CodePen.mp4', 'Pluralsight', 'Steve Michelotti');
+
+INSERT INTO videos (article_id , section_id , video_id , resource_id , author_id ,
+	article_name , section_name , video_name , resource_name , author_name
+) VALUES (1,2,12,1,1,'Building Mobile Apps With the Ionic Framework and AngularJS',
+ 'Getting Started With Ionic', 'Summary.mp4', 'Pluralsight', 'Steve Michelotti');
